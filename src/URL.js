df8db3cf7c20cee3e0d95af612c205e24e49cf8a
Ananth <110927979+Ananththandavarayamurthi@users.noreply.github.com>
2023-07-18 11:01:03 +0530

    Update URL.js

--- src/URL.js
+++ src/URL.js
@@ -1,2 +1,2 @@
 export const empurl="https://cstreambaackfirst.onrender.com/api/employees"
-export const orgurl="https://cstreambaackfirst.onrender.com/api/orngaizations"
+export const orgurl="https://cstreambaackfirst.onrender.com/api/organizations"
